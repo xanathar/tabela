80dedbd9ec65bd6c7ef1e621da3a1dd13c2c7513
Marco Mastropaolo <marcomp@amazon.com>
2025-03-20 11:38:45 +0100

    Check script ignores POT-Creation-Date spurious changes

--- check.sh
+++ check.sh
@@ -1,14 +1,33 @@
 #! /bin/bash
 set -e
 
+fix-translation-of-file () {
+    if git diff --ignore-matching-lines='POT-Creation-Date' --exit-code "$1" ; then
+        git restore "$1"
+    fi
+}
+
+fix-translations () {
+    for i in "po/"*.po; do
+        fix-translation-of-file "$i"
+    done
+    fix-translation-of-file 'po/tabela.pot'
+}
+
+
 meson setup _build --prefix "$(pwd)/_install"
+
 meson compile tabela-pot -C _build
 meson compile tabela-gmo -C _build
 meson compile tabela-update-po -C _build
+
+fix-translations
+
 meson compile -C _build
 meson install -C _build
 meson compile cargo-clippy -C _build
 cargo fmt
+
 echo ''
 echo '-------------------------------------------------------'
 echo ''
