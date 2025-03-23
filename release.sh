ad8b426020c9ef469a0eaca3a45070e0138f9a67
xanathar <marco@mastropaolo.com>
2025-03-23 12:45:02 +0100

    Release script seems ok

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-set -e
+set -eu
 
 APP_ID="com.mastropaolo.www.tabela"
 
@@ -21,55 +21,79 @@ warning () {
     echo -e "${WARNING}$1${CLEAR}"
 }
 
-# if [ ! "$(which jq)" ]; then
-#     die "jq not found in path. Please install jq then try again."
-# fi
-# 
-# if [ ! "$(which gh)" ]; then
-#     die "gh not found in path. Please install github cli then try again."
-# fi
-# 
-# git diff --exit-code >/dev/null 2>&1 || die "Repo is dirty, please commit & push changes."
-# git merge-base --is-ancestor HEAD '@{u}' 2>&1 || die "Changes have not been pushed."
+prereq () {
+    if [ ! "$(which "$1")" ]; then
+        die "'$1' not found in path. Please install '$1' then try again."
+    fi
+}
+
+info "Checking prerequisite programs are installed"
+
+prereq flatpak
+prereq jq
+prereq gh
+
+info "Checking repo is clean and pushed upstream"
+
+git diff --exit-code >/dev/null 2>&1 || die "Repo is dirty, please commit & push changes."
+git merge-base --is-ancestor HEAD '@{u}' 2>&1 || die "Changes have not been pushed."
+
+info "Checking version is valid"
 
 VERSION="$(meson introspect meson.build --projectinfo | jq -r '.version')"
 TAG="v$VERSION"
 
-# if [ "$(git tag -l "$TAG")" ]; then
-#     echo "Tag $TAG already exists -- bailing out. Change the version in the meson file and start again."
-#     exit 1
-# fi
-# 
-# warning "Releasing version $VERSION"
-# warning "Press Ctrl+C in 5 seconds to abort"
-# echo "5..."
-# sleep 1
-# echo "4..."
-# sleep 1
-# echo "3..."
-# sleep 1
-# echo "2..."
-# sleep 1
-# echo "1..."
-# sleep 1
-# echo "GO!"
-# 
-# rm -rf _build
-# 
-# ./check.sh
-# 
-# meson dist -C _build
-# 
-# git tag "$TAG"
-# git push origin tag "$TAG"
-# 
-# gh release create "$TAG" --generate-notes "_build/meson-dist/tabela-$VERSION.tar.xz"
+if [ "$(git tag -l "$TAG")" ]; then
+    echo "Tag $TAG already exists -- bailing out. Change the version in the meson file and start again."
+    exit 1
+fi
+
+echo ""
+echo ""
+
+warning "Releasing version $VERSION"
+warning "Press Ctrl+C in 5 seconds to abort"
+echo "5..."
+sleep 1
+echo "4..."
+sleep 1
+echo "3..."
+sleep 1
+echo "2..."
+sleep 1
+echo "1..."
+sleep 1
+echo "GO!"
+
+info "Cleaning working directories..."
+
+rm -rf _build
+rm -rf builddir
+rm -rf repo
+
+info "Running check script..."
+
+./check.sh
+
+info "Creating tarball..."
+
+meson dist -C _build
+
+info "Creating tag..."
+
+git tag "$TAG"
+git push origin tag "$TAG"
+
+info "Creating release..."
+
+gh release create "$TAG" --generate-notes "_build/meson-dist/tabela-$VERSION.tar.xz"
+
+info "Creating manifest..."
 
 URL=$(gh release view v0.1.2 --json assets | jq ".assets[0].url" -r)
 HASH=$(cut "_build/meson-dist/tabela-$VERSION.tar.xz.sha256sum" -b 1-64)
 
 m4 -DTARBALL_URL="$URL" -DTARBALL_HASH="$HASH" "build-aux/$APP_ID.json.in" > "_build/$APP_ID.json"
-
 
 info "Building flatpak..."
 
@@ -79,8 +103,13 @@ info "Linting flatpak..."
 
 flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest "_build/$APP_ID.json"
 
-info "Linting user repo..."
+info "Linting flatpak repo..."
 
-flatpak run --command=flatpak-builder-lint org.flatpak.Builder repo repo
+if flatpak run --command=flatpak-builder-lint org.flatpak.Builder repo repo | jq ".errors" | grep -Ev '(\[|\]|appstream-screenshots-not-mirrored-in-ostree)'; then
+    echo -e "${ERROR}Validation failed:${CLEAR}"
+    flatpak run --command=flatpak-builder-lint org.flatpak.Builder repo repo
+    exit 1
+fi
 
+info "Done."
 
